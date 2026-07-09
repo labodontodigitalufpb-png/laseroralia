b88af117b6f463ea07277b894cb43e08625773e7
Paulo Bonan <paulobonan@Paulos-MacBook-Air.local>
2026-07-09 13:52:04 -0300

    Simplifica topo mobile

--- sw.js
+++ sw.js
@@ -1,9 +1,9 @@
-const CACHE_NAME = "laser-oral-aid-v13";
+const CACHE_NAME = "laser-oral-aid-v14";
 const ASSETS = [
   "./",
   "./index.html",
-  "./styles.css?v=13",
-  "./app.js?v=13",
+  "./styles.css?v=14",
+  "./app.js?v=14",
   "./manifest.webmanifest",
   "./icon.svg",
   "./assets/ufal.jpg",
