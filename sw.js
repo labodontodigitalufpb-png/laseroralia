d1ef2d7a7d8562ac419e3255550ccc39a042354d
Paulo Bonan <paulobonan@Paulos-MacBook-Air.local>
2026-07-09 14:04:38 -0300

    Reposiciona logos institucionais na home

--- sw.js
+++ sw.js
@@ -1,9 +1,9 @@
-const CACHE_NAME = "laser-oral-aid-v16";
+const CACHE_NAME = "laser-oral-aid-v17";
 const ASSETS = [
   "./",
   "./index.html",
-  "./styles.css?v=16",
-  "./app.js?v=16",
+  "./styles.css?v=17",
+  "./app.js?v=17",
   "./manifest.webmanifest",
   "./icon.svg",
   "./assets/ufal.jpg",
